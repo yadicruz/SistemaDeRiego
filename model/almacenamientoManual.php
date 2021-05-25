32ad2ed516991dbf6bdd5d126f76d526c917a27c
yadira-cruz <84687055+yadira-cruz@users.noreply.github.com>
2021-05-25 15:16:42 -0500

    Ingresando conexión a la bd

--- model/almacenamientoManual.php
+++ model/almacenamientoManual.php
@@ -1,3 +1,10 @@
 <?php
+     $servername = "localhost";
+     // Your Database name
+     $dbname = "esp_data";
+     // Your Database user
+     $username = "root";
+     // Your Database user password
+     $password = "";
 
 ?>
