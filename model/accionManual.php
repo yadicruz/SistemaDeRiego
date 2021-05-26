259b237e084c5538eb75ba51dd8023748bb05161
Yadira <yhadi1996@gmail.com>
2021-05-25 22:34:46 -0500

    Actualizando el control manual

--- model/accionManual.php
+++ model/accionManual.php
@@ -1,4 +1,40 @@
 /*Iniciando formato de php*/
 <?php
 
+include_once('almacenamientoManual.php');
+ 
+$action = $id = $name = $gpio = $state = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $action = test_input($_GET["action"]);
+    if ($action == "outputs_state") {
+        $board = test_input($_GET["board"]);
+        $result = getAllOutputStates($board);
+        if ($result) {
+            while ($row = $result->fetch_assoc()) {
+                $rows[$row["gpio"]] = $row["state"];
+            }
+        }
+        echo json_encode($rows);
+        
+    }
+    else if ($action == "output_update") {
+        $id = test_input($_GET["id"]);
+        $state = test_input($_GET["state"]);
+        $result = updateOutput($id, $state);
+        echo $result;
+    }
+    
+    else {
+        echo "Invalid HTTP request.";
+    }
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 ?>
--- model/almacenamientoManual.php
+++ model/almacenamientoManual.php
@@ -1,5 +1,82 @@
 <?php
-     
 
+   include_once('db.php');
 
+   function updateOutput($id, $state) {
+       include_once('conn.php');
+
+       $sql = "UPDATE Outputs SET state='" . $state . "' WHERE id='". $id .  "'";
+      if ($conn->query($sql) === TRUE) {
+           return "Actualización exitosa";
+       }
+       else {
+           return "Error: " . $sql . "<br>" . $conn->error;
+       }
+       $conn->close();
+   }
+
+   function getAllOutputs() {
+    include_once('conn.php');
+
+       $sql = "SELECT id, name, board, gpio, state FROM Outputs  WHERE id = 5 ORDER BY board";
+       if ($result = $conn->query($sql)) {
+           return $result;
+       }
+       else {
+           return false;
+       }
+       $conn->close();
+   }
+
+   function getAllOutputs2() {
+    include_once('conn.php');
+
+       $sql = "SELECT id, name, board, gpio, state FROM Outputs  WHERE id = 8 ORDER BY board";
+       if ($result = $conn->query($sql)) {
+           return $result;
+       }
+       else {
+           return false;
+       }
+       $conn->close();
+   }
+
+   function getAllOutputs3() {
+    include_once('conn.php');
+
+       $sql = "SELECT id, name, board, gpio, state FROM Outputs  WHERE id = 9 ORDER BY board";
+       if ($result = $conn->query($sql)) {
+           return $result;
+       }
+       else {
+           return false;
+       }
+       $conn->close();
+   }
+   function getAllOutputs4() {
+    include_once('conn.php');
+
+       $sql = "SELECT id, name, board, gpio, state, fecha,fin FROM Outputs  WHERE id = 10 ORDER BY board";
+       if ($result = $conn->query($sql)) {
+           return $result;
+       }
+       else {
+           return false;
+       }
+      
+       $conn->close();
+   }
+
+   function getAllOutputStates($board) {
+    include_once('conn.php');
+
+       $sql = "SELECT gpio, state FROM Outputs WHERE board='" . $board . "'";
+       if ($result = $conn->query($sql)) {
+           return $result;
+       }
+       else {
+           return false;
+       }
+       $conn->close();
+   }
 ?>
