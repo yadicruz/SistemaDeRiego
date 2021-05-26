43a2d1392e9d260b94f0a1b64bee937c909b8d23
yadira-cruz <84687055+yadira-cruz@users.noreply.github.com>
2021-05-25 19:50:16 -0500

    Update almacenamientoManual.php

--- model/almacenamientoManual.php
+++ model/almacenamientoManual.php
@@ -1,25 +1,5 @@
 <?php
-     //nombre del servidor
-     $servername = "localhost";
-     // nombre de la bd
-     $dbname = "esp_data";
-     // usuario del servidor
-     $username = "root";
-     //contraseña del servidor
-     $password = "";
-
-function conexion() {
-        global $servername, $username, $password, $dbname;
-        // Realizar conexión
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        //Confirmar conexión
-        if ($conn->connect_error) {
-            die("Conexión fallada: " . $conn->connect_error);
-        }
-      
-        $conn->close();
-    }
-conexion();
+     
 
 
 ?>
