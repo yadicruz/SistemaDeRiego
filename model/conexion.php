e6cf6e78cca3db441db6c6f13247dac3cc5f87fb
yadira-cruz <84687055+yadira-cruz@users.noreply.github.com>
2021-05-25 19:46:18 -0500

    Actualización de los parámetros para la BD

---
+++ model/conexion.php
@@ -0,0 +1,10 @@
+  <?php
+  //nombre del servidor
+     $servername = "localhost";
+     // nombre de la bd
+     $dbname = "esp_data";
+     // usuario del servidor
+     $username = "root";
+     //contraseña del servidor
+     $password = "";
+?>
